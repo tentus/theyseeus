9b93c648595b898d996ba196b0ef1315aab70285
Thomas Holden <tentus@gmail.com>
2018-02-16 21:15:45 -0500

    rough in placeholder tileset assets

---
+++ maps/bg_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="bg_tileset" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="../assets/tilesets/bg_tileset.png" width="512" height="512"/>
+</tileset>
