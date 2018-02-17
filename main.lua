b2253a84b5a5c45492f2ac1703c1c726948e0ef9
Thomas Holden <tentus@gmail.com>
2018-02-16 20:35:19 -0500

    rough in splash scene, add helper to load for requiring an entire dir

--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
 Class       = require "libraries.hump.class"
 Gamestate   = require "libraries.hump.gamestate"
 sti         = require "libraries.sti"
+
+function love.load()
+    local requireDir = function(dir)
+        local files = love.filesystem.getDirectoryItems(dir)
+        for _, file in pairs(files) do
+            require(dir .. '/' .. string.gsub( file, ".lua", ""))
+        end
+    end
+
+    requireDir('scenes')
+
+    Gamestate.registerEvents()
+    Gamestate.switch(SplashScene)
+end
---
+++ scenes/splash_scene.lua
@@ -0,0 +1,50 @@
+SplashScene = {
+    step = 0,
+    time = {
+        passed = 0,
+        limit = 4,
+    },
+    logos = {
+        love.graphics.newImage("assets/logos/beardville_logo.png"),
+        love.graphics.newImage("assets/logos/love_logo.png"),
+    },
+}
+
+function SplashScene:init()
+    self:proceed()
+end
+
+function SplashScene:update(dt)
+    self.time.passed = self.time.passed + dt
+    if self.time.passed >= self.time.limit then
+        self:proceed()
+    end
+end
+
+function SplashScene:draw()
+    local screenWidth, screenHeight = love.window.getMode()
+    local x = (screenWidth / 2) - (self.imageWidth / 2)
+    local y = (screenHeight / 2) - (self.imageHeight / 2)
+    love.graphics.draw(
+        self.image,
+        x,
+        y
+    )
+end
+
+function SplashScene:keypressed()
+    self:proceed()
+end
+
+function SplashScene:proceed()
+    if self.step < #self.logos then
+        self.time.passed = 0
+        self.step = self.step + 1
+        self:updateImageProperties()
+    end
+end
+
+function SplashScene:updateImageProperties()
+    self.image = self.logos[self.step]
+    self.imageWidth, self.imageHeight = self.image:getDimensions()
+end
