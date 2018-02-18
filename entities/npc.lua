41350560abd970427a9ea7c6ec4823209e72e637
Thomas Holden <tentus@gmail.com>
2018-02-17 22:13:26 -0500

    use pathfinding in npcs to make them chase the player

--- entities/npc.lua
+++ entities/npc.lua
@@ -1,3 +1,29 @@
 NPC = Class{
     __includes = Minotaur,
+    pathnode = {
+        x = 0,
+        y = 0,
+    },
+    force = 4000,
 }
+
+function NPC:update(dt)
+    local x, y = self.body:getPosition()
+    local px, py = WorldScene.player.body:getPosition()
+
+    -- we recalc constantly because we're constantly catching up to our destination
+    self.pathnode = WorldScene.pathManager:getNextPathNode(x, y, px, py)
+
+    -- we don't apply force unless we're far enough from it to be worthwhile, to avoid jitter
+    local threshold = 4
+    local h, v = 0, 0
+    if math.abs(self.pathnode.x - x) > threshold then
+        if self.pathnode.x < x then h = -1 end
+        if self.pathnode.x > x then h = 1 end
+    end
+    if math.abs(self.pathnode.y - y) > threshold then
+        if self.pathnode.y < y then v = -1 end
+        if self.pathnode.y > y then v = 1 end
+    end
+    self.body:applyForce(h * self.force, v * self.force)
+end
