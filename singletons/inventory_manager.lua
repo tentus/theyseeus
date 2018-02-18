bbd6ff48acda574d7ed25da8ea6ee59a69c975d6
Thomas Holden <tentus@gmail.com>
2018-02-18 13:47:24 -0500

    move the inventory rendering, to avoid overlap

--- singletons/inventory_manager.lua
+++ singletons/inventory_manager.lua
@@ -4,10 +4,19 @@ InventoryManager = {
 
     -- overall count of yarns, for easier reference
     count = 0,
+    image = love.graphics.newImage("assets/sprites/yarn.png"),
+    font = love.graphics.newFont(16),
 }
 
 function InventoryManager:draw()
-    love.graphics.print('Yarns Collected: ' .. self.count)
+    local height = love.graphics.getHeight()
+    local reset = love.graphics.getFont()
+
+    love.graphics.draw(self.image, 8, height - 40, 0, 0.5, 0.5)
+
+    love.graphics.setFont(self.font)
+    love.graphics.print(' x ' .. self.count, 40, height - 32)
+    love.graphics.setFont(reset)
 end
 
 function InventoryManager:collectYarn(regionX, regionY)
