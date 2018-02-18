c38619f6342e4ce64204c628b9899b9f5ae8a33a
Thomas Holden <tentus@gmail.com>
2018-02-18 17:05:10 -0500

    use local node for npc pathing

--- entities/npc.lua
+++ entities/npc.lua
@@ -1,10 +1,6 @@
 NPC = Class{
     __includes = {Minotaur, Damagable, Killable},
     classname = 'NPC',
-    pathnode = {
-        x = 0,
-        y = 0,
-    },
     force = 4000,
     sleeping = true,
     hearing = {},
@@ -29,18 +25,18 @@ function NPC:update(dt)
     local px, py = WorldScene.player.body:getPosition()
 
     -- we recalc constantly because we're constantly catching up to our destination
-    self.pathnode = WorldScene.pathManager:getNextPathNode(x, y, px, py)
+    local node = WorldScene.pathManager:getNextPathNode(x, y, px, py)
 
     -- we don't apply force unless we're far enough from it to be worthwhile, to avoid jitter
     local threshold = 4
     local h, v = 0, 0
-    if math.abs(self.pathnode.x - x) > threshold then
-        if self.pathnode.x < x then h = -1 end
-        if self.pathnode.x > x then h = 1 end
+    if math.abs(node.x - x) > threshold then
+        if node.x < x then h = -1 end
+        if node.x > x then h = 1 end
     end
-    if math.abs(self.pathnode.y - y) > threshold then
-        if self.pathnode.y < y then v = -1 end
-        if self.pathnode.y > y then v = 1 end
+    if math.abs(node.y - y) > threshold then
+        if node.y < y then v = -1 end
+        if node.y > y then v = 1 end
     end
     self.body:applyForce(h * self.force, v * self.force)
 end
