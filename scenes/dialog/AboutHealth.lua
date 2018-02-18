b53e7ac000f74b9b15b38a36622434fc5e6d9161
Thomas Holden <tentus@gmail.com>
2018-02-18 15:55:18 -0500

    add some signs to the home region, and their corresponding dialogs

---
+++ scenes/dialog/AboutHealth.lua
@@ -0,0 +1,8 @@
+return {
+    {
+        "You have a limited amount of health, which is indicated in the top left.",
+    },
+    {
+        "If you run out of health you're respawn in this area.",
+    },
+}
