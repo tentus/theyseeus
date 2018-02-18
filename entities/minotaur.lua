b2f7c35efc77ab5d55a280e4922234b79ad39768
Thomas Holden <tentus@gmail.com>
2018-02-18 00:17:25 -0500

    fix minotaur mass at 1, set self on fixture userdata

--- entities/minotaur.lua
+++ entities/minotaur.lua
@@ -40,8 +40,11 @@ end
 function Minotaur:createBody(world, x, y)
     self.body = love.physics.newBody(world, x or 0, y or 0, "dynamic")
     self.body:setLinearDamping(10)
-    self.body:setMass(75)
 
     self.shape   = love.physics.newCircleShape(self.radius)
     self.fixture = love.physics.newFixture(self.body, self.shape)
+    self.fixture:setUserData(self)
+
+    -- each shape affixed will affect the mass, so we have to reset it each time
+    self.body:setMass(1)
 end
