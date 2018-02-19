030474273683a8b5c2eb07b2f5c95a10698b0a6b
Thomas Holden <tentus@gmail.com>
2018-02-19 00:15:14 -0500

    add Upgrade ent

---
+++ entities/upgrade.lua
@@ -0,0 +1,44 @@
+Upgrade = Class{
+    __includes = Killable,
+    classname = 'Upgrade',
+    radius = 32,
+    sprite = {
+        image = love.graphics.newImage("assets/sprites/upgrade.png"),
+    },
+    glow = GlowEffect()
+}
+
+function Upgrade:init(x, y, world)
+    -- set offsets for image drawing, since it won't change
+    self.sprite.x = self.sprite.image:getWidth() / 2
+    self.sprite.y = self.sprite.image:getHeight() / 2
+
+    self:createBody(world, x, y)
+end
+
+function Upgrade:draw()
+    local x, y = self.body:getPosition()
+    self.glow:draw(x, y)
+    love.graphics.draw(self.sprite.image, x - self.sprite.x, y - self.sprite.y)
+end
+
+function Upgrade:update(dt)
+    self.glow:update(dt)
+end
+
+function Upgrade:createBody(world, x, y)
+    self.body = love.physics.newBody(world, x or 0, y or 0, "static")
+
+    self.shape   = love.physics.newCircleShape(self.radius)
+    self.fixture = love.physics.newFixture(self.body, self.shape)
+    self.fixture:setSensor(true)
+    self.fixture:setUserData(self)
+end
+
+function Upgrade:beginContact(other)
+    if other.classname == "Player" then
+        InventoryManager:collectUpgrade(RegionManager:coords())
+        other:incrementHealth()
+        self:kill()
+    end
+end
