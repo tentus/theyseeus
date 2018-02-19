828da35ae6e27f78dba5fdf195f550dc6db724ba
Thomas Holden <tentus@gmail.com>
2018-02-19 18:48:26 -0500

    split size in sign into width and height

--- entities/sign.lua
+++ entities/sign.lua
@@ -4,14 +4,15 @@ Sign = Class{
     dialog = "",
 
     -- our body the same dimensions as a normal tile
-    size = 64,
+    width = 64,
+    height = 64,
     image = love.graphics.newImage("assets/sprites/sign.png"),
 }
 
 function Sign:init(x, y, world, dialog)
     -- the pins in Tiled look higher than they are, so we push ourselves up a little
     self.body = love.physics.newBody(world, x, y - 16, "static")
-    self.shape   = love.physics.newRectangleShape(self.size, self.size)
+    self.shape   = love.physics.newRectangleShape(self.width, self.height)
     self.fixture = love.physics.newFixture(self.body, self.shape)
     self.fixture:setUserData(self)
 
@@ -20,7 +21,7 @@ end
 
 function Sign:draw()
     local x, y = self.body:getPosition()
-    love.graphics.draw(self.image, x - (self.size / 2), y - (self.size / 2))
+    love.graphics.draw(self.image, x - (self.width / 2), y - (self.height / 2))
 end
 
 function Sign:playerContact(other)
