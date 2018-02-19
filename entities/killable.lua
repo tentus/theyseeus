1e18b351dd0f6d7eaa38a4f081b94120e39d1f9c
Thomas Holden <tentus@gmail.com>
2018-02-19 17:19:55 -0500

    add death audio

--- entities/killable.lua
+++ entities/killable.lua
@@ -1,7 +1,9 @@
 Killable = Class{
-    dead = false
+    dead = false,
+    death_audio = love.audio.newSource("assets/audio/die.ogg", "static"),
 }
 
 function Killable:kill()
     self.dead = true
+    self.death_audio:play()
 end
