e10169e57f3faf8417423b0ec6cdd31821eb7a44
Thomas Holden <tentus@gmail.com>
2018-02-19 18:29:01 -0500

    implement an Interactable behavior for things only the player can interact with

---
+++ entities/behaviors/interactable.lua
@@ -0,0 +1,13 @@
+-- a shortcut property for things only the player can interact with
+Interactable = Class{
+}
+
+function Interactable:playerContact(player)
+    -- interface placeholder
+end
+
+function Interactable:beginContact(other)
+    if other.classname == "Player" then
+        self:playerContact(other)
+    end
+end
