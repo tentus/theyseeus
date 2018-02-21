58892e4042e615b777d37675d7efb39f3b0f985f
Thomas Holden <tentus@gmail.com>
2018-02-21 01:46:06 -0500

    expand the background tileset to have more options, update tiles affected

--- maps/bg_tileset.tsx
+++ maps/bg_tileset.tsx
@@ -3,44 +3,44 @@
  <image source="../assets/tilesets/bg_tileset.png" width="512" height="512"/>
  <terraintypes>
   <terrain name="Grass" tile="9"/>
-  <terrain name="Dirt" tile="33"/>
-  <terrain name="Tile" tile="12"/>
+  <terrain name="Dirt" tile="32"/>
+  <terrain name="Tiles" tile="4"/>
   <terrain name="Swamp" tile="36"/>
  </terraintypes>
  <tile id="0" terrain=",,,0"/>
  <tile id="1" terrain=",,0,0"/>
  <tile id="2" terrain=",,0,"/>
- <tile id="3" terrain=",,,2"/>
- <tile id="4" terrain=",,2,2"/>
- <tile id="5" terrain=",,2,"/>
+ <tile id="4" terrain=",,,2"/>
+ <tile id="5" terrain=",,2,2"/>
+ <tile id="6" terrain=",,2,"/>
  <tile id="8" terrain=",0,,0"/>
  <tile id="9" terrain="0,0,0,0"/>
  <tile id="10" terrain="0,,0,"/>
- <tile id="11" terrain=",2,,2"/>
- <tile id="12" terrain="2,2,2,2"/>
- <tile id="13" terrain="2,,2,"/>
+ <tile id="12" terrain=",2,,2"/>
+ <tile id="13" terrain="2,2,2,2"/>
+ <tile id="14" terrain="2,,2,"/>
  <tile id="16" terrain=",0,,"/>
  <tile id="17" terrain="0,0,,"/>
  <tile id="18" terrain="0,,,"/>
- <tile id="19" terrain=",2,,"/>
- <tile id="20" terrain="2,2,,"/>
- <tile id="21" terrain="2,,,"/>
- <tile id="24" terrain=",,,1"/>
- <tile id="25" terrain=",,1,1"/>
- <tile id="26" terrain=",,1,"/>
- <tile id="27" terrain=",,,3"/>
- <tile id="28" terrain=",,3,3"/>
- <tile id="29" terrain=",,3,"/>
- <tile id="32" terrain=",1,,1"/>
- <tile id="33" terrain="1,1,1,1"/>
- <tile id="34" terrain="1,,1,"/>
- <tile id="35" terrain=",3,,3"/>
- <tile id="36" terrain="3,3,3,3"/>
- <tile id="37" terrain="3,,3,"/>
- <tile id="40" terrain=",1,,"/>
- <tile id="41" terrain="1,1,,"/>
- <tile id="42" terrain="1,,,"/>
- <tile id="43" terrain=",3,,"/>
- <tile id="44" terrain="3,3,,"/>
- <tile id="45" terrain="3,,,"/>
+ <tile id="20" terrain=",2,,"/>
+ <tile id="21" terrain="2,2,,"/>
+ <tile id="22" terrain="2,,,"/>
+ <tile id="32" terrain=",,,1"/>
+ <tile id="33" terrain=",,1,1"/>
+ <tile id="34" terrain=",,1,"/>
+ <tile id="36" terrain=",,,3"/>
+ <tile id="37" terrain=",,3,3"/>
+ <tile id="38" terrain=",,3,"/>
+ <tile id="40" terrain=",1,,1"/>
+ <tile id="41" terrain="1,1,1,1"/>
+ <tile id="42" terrain="1,,1,"/>
+ <tile id="44" terrain=",3,,3"/>
+ <tile id="45" terrain="3,3,3,3"/>
+ <tile id="46" terrain="3,,3,"/>
+ <tile id="48" terrain=",1,,"/>
+ <tile id="49" terrain="1,1,,"/>
+ <tile id="50" terrain="1,,,"/>
+ <tile id="52" terrain=",3,,"/>
+ <tile id="53" terrain="3,3,,"/>
+ <tile id="54" terrain="3,,,"/>
 </tileset>
