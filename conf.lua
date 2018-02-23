411c195e421d6afce227dbbfac91bcbfb9ecfd70
Thomas Holden <tentus@gmail.com>
2018-02-22 23:28:02 -0500

    allow window resizing

--- conf.lua
+++ conf.lua
@@ -1,6 +1,7 @@
 function love.conf(t)
     t.version = "0.10.2"
     t.window.icon = "assets/icon.png"
+    t.window.resizable = true
     t.window.title = "They See Us"
     t.window.width = 1280
     t.window.height = 720
