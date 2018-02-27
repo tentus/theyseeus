fca72c668c92c75de80ea32c8580619d93773f93
Thomas Holden <tentus@gmail.com>
2018-02-26 22:45:05 -0500

    add coins to HUD

--- singletons/hud.lua
+++ singletons/hud.lua
@@ -4,6 +4,7 @@ HUD = {
         health = love.graphics.newImage("assets/sprites/hud/health_icon.png"),
         damage = love.graphics.newImage("assets/sprites/hud/damage_icon.png"),
         yarn   = love.graphics.newImage("assets/sprites/hud/yarn_icon.png"),
+        coin   = love.graphics.newImage("assets/sprites/hud/coin_icon.png"),
     }
 }
 
@@ -25,4 +26,7 @@ function HUD:draw()
     -- yarn and coins total in bottom left
     love.graphics.draw(self.icons.yarn, 8, height - 40)
     love.graphics.print(' x ' .. InventoryManager:total(Yarn.classname), 40, height - 32)
+
+    love.graphics.draw(self.icons.coin, 8, height - 80)
+    love.graphics.print(' x ' .. InventoryManager:total(Coin.classname), 40, height - 64)
 end
