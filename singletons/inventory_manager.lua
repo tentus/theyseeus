ec867a09e879c26c5c1474170764108be39facd9
Thomas Holden <tentus@gmail.com>
2018-02-26 20:33:27 -0500

    make inventory less hardcode-y by using fallbacks in lookups

--- singletons/inventory_manager.lua
+++ singletons/inventory_manager.lua
@@ -1,21 +1,20 @@
 InventoryManager = {
     -- which items have been collected (keyed by region coords)
     collected = {
-        Yarn = {},
-        Upgrade = {},
-        Map = {},
-        Gem = {},
     }
 }
 
 function InventoryManager:collect(thing)
+    if not self.collected[thing] then
+        self.collected[thing] = {}
+    end
     self.collected[thing][self:key(RegionManager:coords())] = true
     Logger:add(thing .. " Collected")
     return self:total(thing)
 end
 
 function InventoryManager:has(thing, regionX, regionY)
-    return self.collected[thing][self:key(regionX, regionY)] == true
+    return self.collected[thing] and self.collected[thing][self:key(regionX, regionY)] == true
 end
 
 function InventoryManager:reset()
@@ -26,7 +25,7 @@ end
 
 function InventoryManager:total(thing)
     local count = 0
-    for _ in pairs(self.collected[thing]) do count = count + 1 end
+    for _ in pairs(self.collected[thing] or {}) do count = count + 1 end
     return count
 end
 
