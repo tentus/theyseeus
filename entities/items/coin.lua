67228b0dccee3c8f4dbffd8c37fc0c1dd824152d
Thomas Holden <tentus@gmail.com>
2018-02-26 22:00:31 -0500

    rough in coin item entity

---
+++ entities/items/coin.lua
@@ -0,0 +1,20 @@
+Coin = Class{
+    __includes = {Killable, Sensable},
+    classname = 'Coin',
+    radius = 16,
+    sprite = Sprite("assets/sprites/items/coin.png"),
+    death_audio = "assets/audio/collect.ogg",
+}
+
+function Coin:init(x, y)
+    Sensable.init(self, x, y)
+end
+
+function Coin:draw()
+    self.sprite:draw(self.x, self.y)
+end
+
+function Coin:playerSensed()
+    InventoryManager:collect(self.classname)
+    self:kill()
+end
