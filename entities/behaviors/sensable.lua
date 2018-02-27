5435db960cc27243c3a00836f4f0977cf3d8fce7
Thomas Holden <tentus@gmail.com>
2018-02-27 14:59:00 -0500

    rm errant print

--- entities/behaviors/sensable.lua
+++ entities/behaviors/sensable.lua
@@ -12,7 +12,6 @@ function Sensable:update(dt)
     local player = WorldScene.player
     local x, y = player.body:getPosition()
     if math.sqrt((self.x - x) ^ 2 + (self.y - y) ^ 2) <= (self.radius + player.radius) then
-        print(x, y)
         self:playerSensed(player)
     end
 end
