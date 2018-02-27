cef68d3efa7f9c8adcb3053a6a2a3a01b961c359
Thomas Holden <tentus@gmail.com>
2018-02-26 21:52:13 -0500

    reorganize icons into a hud subdir

--- singletons/hud.lua
+++ singletons/hud.lua
@@ -1,9 +1,9 @@
 HUD = {
     show = true,
     icons = {
-        health = love.graphics.newImage("assets/sprites/health_icon.png"),
-        damage = love.graphics.newImage("assets/sprites/damage_icon.png"),
-        yarn   = love.graphics.newImage("assets/sprites/items/yarn.png"),
+        health = love.graphics.newImage("assets/sprites/hud/health_icon.png"),
+        damage = love.graphics.newImage("assets/sprites/hud/damage_icon.png"),
+        yarn   = love.graphics.newImage("assets/sprites/hud/yarn_icon.png"),
     }
 }
 
@@ -20,8 +20,9 @@ function HUD:draw()
         love.graphics.draw(icon, (i * 40) - 32, 8)
     end
 
-    -- yarn total in bottom left
     local height = love.graphics.getHeight()
+
+    -- yarn and coins total in bottom left
     love.graphics.draw(self.icons.yarn, 8, height - 40, 0, 0.5, 0.5)
-    love.graphics.print(' x ' .. InventoryManager:total("Yarn"), 40, height - 32)
+    love.graphics.print(' x ' .. InventoryManager:total(Yarn.classname), 40, height - 32)
 end
