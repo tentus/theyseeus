a48bf9d1092e174da80b20eccb83cb72b776b0db
Thomas Holden <tentus@gmail.com>
2018-03-01 02:19:04 -0500

    check death audio hasn't been disabled before playing it

--- entities/behaviors/killable.lua
+++ entities/behaviors/killable.lua
@@ -5,5 +5,7 @@ Killable = Class{
 
 function Killable:kill()
     self.dead = true
-    AudioManager:play(self.death_audio)
+    if self.death_audio then
+        AudioManager:play(self.death_audio)
+    end
 end
