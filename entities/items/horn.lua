91f8d43c8f619c4922dcdfc6b615a3f28d4d2eb3
Thomas Holden <tentus@gmail.com>
2018-03-01 23:57:23 -0500

    rough in horn entity

---
+++ entities/items/horn.lua
@@ -0,0 +1,19 @@
+Horn = Class{
+    __includes = {Killable, Sensable},
+    classname = 'Horn',
+    sprite = Sprite("assets/sprites/items/horn.png"),
+    death_audio = "assets/audio/collect.ogg",
+}
+
+function Horn:init(x, y)
+    Sensable.init(self, x, y)
+end
+
+function Horn:draw()
+    self.sprite:draw(self.x, self.y)
+end
+
+function Horn:playerSensed()
+    InventoryManager:collect(self.classname)
+    self:kill()
+end
