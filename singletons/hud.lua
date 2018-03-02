471525df36560c0878b0de34e9c1722347e5a479
Thomas Holden <tentus@gmail.com>
2018-03-01 23:58:31 -0500

    add "hearing" asset and use it in the HUD to hint at enemy positions

--- singletons/hud.lua
+++ singletons/hud.lua
@@ -1,11 +1,12 @@
 HUD = {
     show = true,
+    hearing = love.graphics.newImage("assets/sprites/effects/hearing.png"),
     icons = {
         health = love.graphics.newImage("assets/sprites/hud/health_icon.png"),
         damage = love.graphics.newImage("assets/sprites/hud/damage_icon.png"),
         yarn   = love.graphics.newImage("assets/sprites/hud/yarn_icon.png"),
         coin   = love.graphics.newImage("assets/sprites/hud/coin_icon.png"),
-    }
+    },
 }
 
 function HUD:toggle()
@@ -15,13 +16,29 @@ end
 function HUD:draw()
     if not self.show then return end
 
-    -- health in top left
-    for i=1, WorldScene.player.maxHealth do
-        local icon = ((WorldScene.player.health < i) and self.icons.damage or self.icons.health)
-        love.graphics.draw(icon, (i * 40) - 32, 8)
+    local width, height = love.window.getMode()
+    local player = WorldScene.player
+
+    if InventoryManager:total(Horn.classname) > 0 then
+        local x, y = player.body:getPosition()
+        for _, ent in pairs(WorldScene.map.layers["Minotaurs"].ents) do
+            if ent.body and not ent.body:isDestroyed() then
+                local x2, y2 = ent.body:getPosition()
+                local dist = ((x2 - x) ^ 2 + (y2 - y) ^ 2) ^ 0.5
+                local r = math.atan2(y2 - y, x2 - x)
+
+                love.graphics.setColor(255, 255, 255, math.min((512 / dist) * 128, 255))
+                love.graphics.draw(self.hearing, width / 2, height / 2, r, 1, 1, 0, 64)
+            end
+        end
+        love.graphics.setColor(255, 255, 255)
     end
 
-    local height = love.graphics.getHeight()
+    -- health in top left
+    for i=1, player.maxHealth do
+        local icon = ((player.health < i) and self.icons.damage or self.icons.health)
+        love.graphics.draw(icon, (i * 40) - 32, 8)
+    end
 
     -- yarn and coins total in bottom left
     love.graphics.draw(self.icons.yarn, 8, height - 40)
