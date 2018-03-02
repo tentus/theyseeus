76fc838f7939812a7c2b897ff01dddfdbb4fc58a
Thomas Holden <tentus@gmail.com>
2018-03-02 12:29:45 -0500

    simplify roadsign dialog by referencing RegionManager.cursor directly

--- scenes/dialog/Roadsign.lua
+++ scenes/dialog/Roadsign.lua
@@ -1,17 +1,16 @@
 return {
     {
         function()
-            local rx, ry = RegionManager:coords()
-            local hx, hy = RegionManager.home.x, RegionManager.home.y
+            local c, h = RegionManager.cursor, RegionManager.home
             local a1, a2
-            if ry < hy then
+            if c.y < h.y then
                 a1 = "north"
-            elseif ry > hy then
+            elseif c.y > h.y then
                 a1 = "south"
             end
-            if rx < hx then
+            if c.x < h.x then
                 a2 = "east"
-            elseif rx > hx then
+            elseif c.x > h.x then
                 a2 = "west"
             end
             return "Welcome to the " .. (a1 or "") .. (a2 or "") .. " woodlands."
