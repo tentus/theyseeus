25970ae849a50718bc4c73021ef44ae82051e3e6
Thomas Holden <tentus@gmail.com>
2018-03-03 17:15:17 -0500

    rough in Teleport entity

---
+++ src/entities/teleport.lua
@@ -0,0 +1,32 @@
+Teleport = Class{
+    __includes = {Sensable},
+    classname = 'Teleport',
+    sprite = SpriteComponent('assets/sprites/teleport.png'),
+    pulse = 0,
+    pulseRate = 2.5,
+    destination = {},
+}
+
+function Teleport:init(x, y, dx, dy)
+    Sensable.init(self, x, y)
+    self.destination = {
+        x = dx,
+        y = dy,
+    }
+end
+
+function Teleport:update(dt)
+    Sensable.update(self, dt)
+    self.pulse = self.pulse + (dt / self.pulseRate)
+end
+
+function Teleport:draw()
+    local alpha = (math.abs((self.pulse % 1) - 0.5) * 255) + 128
+    love.graphics.setColor(255, 255, 255, alpha)
+    self.sprite:draw(self.x, self.y)
+    love.graphics.setColor(255, 255, 255)
+end
+
+function Teleport:playerSensed(player)
+    player.body:setPosition(self.destination.x, self.destination.y)
+end
