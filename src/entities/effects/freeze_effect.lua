59a23e814b1b66c7f5b67f1ed4335965a18dbaf1
Thomas Holden <tentus@gmail.com>
2018-03-04 13:40:14 -0500

    add freeze effect

---
+++ src/entities/effects/freeze_effect.lua
@@ -0,0 +1,34 @@
+FreezeEffect = Class{
+    __includes = {Killable},
+    classname = 'Freeze',
+    death_audio = false,
+    image = love.graphics.newImage('assets/sprites/effects/cloud.png'),
+}
+
+function FreezeEffect:init(x, y)
+    local p = love.graphics.newParticleSystem(self.image, 2000)
+    p:setPosition(x, y)
+    p:setEmissionRate(2000)
+    p:setEmitterLifetime(0.1)
+    p:setInsertMode("bottom")
+    p:setParticleLifetime(5)
+    p:setSpin(0, -10)
+    p:setSpeed(1500)
+    p:setSpread(6.3)
+    p:setColors(
+        128, 192, 255, 255,
+        255, 255, 255, 0
+    )
+    self.particles = p
+end
+
+function FreezeEffect:draw()
+    love.graphics.draw(self.particles)
+end
+
+function FreezeEffect:update(dt)
+    self.particles:update(dt)
+    if self.particles:getCount() == 0 then
+        self:kill()
+    end
+end
