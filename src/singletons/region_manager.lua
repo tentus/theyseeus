df6cac89aeb3dccd0f97cc58bc4baff8650e5a59
Thomas Holden <tentus@gmail.com>
2018-03-10 13:44:00 -0500

    add 8th map

--- src/singletons/region_manager.lua
+++ src/singletons/region_manager.lua
@@ -1,7 +1,7 @@
 RegionManager = {
     -- a list of all production-read maps
     whitelist = {
-        1, 2, 3, 4, 5, 6, 7
+        1, 2, 3, 4, 5, 6, 7, 8
     },
 
     -- how many of each pickup we want to create
