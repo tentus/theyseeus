847ba1c8bdb2e40709da0435c891a51017b32067
Thomas Holden <tentus@gmail.com>
2018-03-16 17:33:15 -0400

    rough Physical behavior, which defaults to creating a dynamical, circular body that is not a sensor

---
+++ src/entities/behaviors/physical.lua
@@ -0,0 +1,20 @@
+Physical = Class{
+    -- body    = Body,
+    -- shape   = Shape,
+    -- fixture = Fixture,
+    bodyType = 'dynamic',
+    isSensor = false,
+}
+
+function Physical:createBody(world, x, y)
+    self.body    = love.physics.newBody(world, x, y, self.bodyType)
+    self.shape   = self:makeShape()
+    self.fixture = love.physics.newFixture(self.body, self.shape)
+    self.fixture:setSensor(self.isSensor)
+    self.fixture:setUserData(self)
+end
+
+-- separated so it can be overridden for rectangles
+function Physical:makeShape()
+    return love.physics.newCircleShape(self.radius)
+end
