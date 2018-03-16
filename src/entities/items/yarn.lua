9d28b01610b45714e01c5aae44b4043f8d987e98
Thomas Holden <tentus@gmail.com>
2018-03-16 15:21:45 -0400

    add yarn-specific sfx

--- src/entities/items/yarn.lua
+++ src/entities/items/yarn.lua
@@ -3,4 +3,5 @@ Yarn = Class{
     classname = 'Yarn',
     sprite = SpriteComponent('assets/sprites/items/yarn.png'),
     rippleColor = {128, 128, 255},
+    death_audio = 'assets/audio/yarn.ogg',
 }
