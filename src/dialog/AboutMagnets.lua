de9c928cd120a057c37931840a7f71c82a0cfb70
Thomas Holden <tentus@gmail.com>
2018-03-16 15:19:47 -0400

    add magnet dialog

---
+++ src/dialog/AboutMagnets.lua
@@ -0,0 +1,8 @@
+return {
+    {
+        'Nice, you found an item magnet! These will pull items closer to you if you get within range.',
+    },
+    {
+        'But the question remains, how does it work?',
+    },
+}
