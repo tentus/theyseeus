5b489ab96c1005f0082c86b83ae29d967ff7af6e
Thomas Holden <tentus@gmail.com>
2018-03-16 10:34:13 -0400

    add Explicable behavior

---
+++ src/entities/behaviors/explicable.lua
@@ -0,0 +1,14 @@
+Explicable = Class{
+    --explanationDialog = '',
+}
+
+function Explicable:explain()
+    if self:shouldExplain() then
+        Gamestate.push(DialogScene, self.explanationDialog)
+    end
+end
+
+-- separated for easier overriding
+function Explicable:shouldExplain()
+    return InventoryManager:total(self.classname) == 1
+end
