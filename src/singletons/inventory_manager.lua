a6ab6c2e26935412dc17607e8f72c813a6c4dda7
Thomas Holden <tentus@gmail.com>
2018-03-18 16:49:52 -0400

    rough in "spend" concept for inventory manager

--- src/singletons/inventory_manager.lua
+++ src/singletons/inventory_manager.lua
@@ -6,8 +6,10 @@ end
 
 InventoryManager = {
     -- which items have been collected (keyed by region coords)
-    collected = {
-    }
+    collected = {},
+
+    -- which items (read: coins) have been used (also keyed by region)
+    spent = {},
 }
 
 function InventoryManager:collect(thing, qty)
@@ -19,16 +21,23 @@ function InventoryManager:collect(thing, qty)
     Logger:add(thing .. ' Collected')
 end
 
+function InventoryManager:spend(thing, qty)
+    -- todo
+end
+
 function InventoryManager:has(thing)
     return self.collected[thing] and (self.collected[thing][regionKey()] or 0) > 0
 end
 
 function InventoryManager:total(thing)
-    local count = 0
+    local total = 0
     for _, v in pairs(self.collected[thing] or {}) do
-        count = count + v
+        total = total + v
     end
-    return count
+    for _, v in pairs(self.spent[thing] or {}) do
+        total = total - v
+    end
+    return total
 end
 
 function InventoryManager:list(thing)
